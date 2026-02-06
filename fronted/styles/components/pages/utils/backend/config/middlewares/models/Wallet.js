e1b8d1c6ed18cc211a71c21019765f98fc3c7cc6
Abdoul Wajid Sylla <abdoulwajidsylla@gmail.com>
2026-02-06 02:30:45 +0000

    Netoyage : susppression ancien backend et consolidation à la racine

--- fronted/styles/components/pages/utils/backend/config/middlewares/models/Wallet.js
+++
@@ -1,9 +0,0 @@
-const mongoose = require('mongoose');
-
-const walletSchema = new mongoose.Schema({
-  user_id: { type: mongoose.Schema.Types.ObjectId, ref: 'User', required: true },
-  solde: { type: Number, default: 0 },
-  date_creation: { type: Date, default: Date.now },
-});
-
-module.exports = mongoose.model('Wallet', walletSchema);
