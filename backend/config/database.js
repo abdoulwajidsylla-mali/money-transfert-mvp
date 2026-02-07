af214271323ddfb8d7bf734b60e1b1281ee8c196
Abdoul Wajid Sylla <abdoulwajidsylla@gmail.com>
2026-02-07 02:10:41 +0000

    Backend finalisé et testé

--- backend/config/database.js
+++ backend/config/database.js
@@ -1,11 +1,8 @@
-const mongoose = require('mongoose');
 require('dotenv').config({path: __dirname + '/../.env'});
+const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGO_URI, {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-})
+mongoose.connect(process.env.MONGO_URI)
 .then(() => console.log('MongoDB connecté !'))
-.catch(err => console.log('Erreur MongoDB :',err));
+.catch(err => console.error('Erreur MongoDB :',err));
 
 module.exports = mongoose;
